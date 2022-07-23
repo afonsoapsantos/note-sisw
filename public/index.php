b4128ee58a9872bc998c644b112aa7852a48159c
Afonso Santos <afonsoapsantos@outlook.com>
2022-07-23 13:37:38 -0300

    Sisweb projecto do note

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-require_once("../sisweb/vendor/autoload.php");
+require_once(__DIR__ . '/../vendor/autoload.php');
 
 use Slim\Factory\AppFactory;
 //$app->addErrorMiddleware(false, true, true);
